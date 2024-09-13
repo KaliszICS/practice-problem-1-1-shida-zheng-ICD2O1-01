0696f580c92f0657f65e08d006f24173399b0211
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-09-13 14:14:07 +0000

    b

--- PP1_1.py
+++ PP1_1.py
@@ -8,8 +8,8 @@ def q2():
 def q3():
   #Write Assignment code here
   print(" Help Me!")  
-def q4:
-4  #Write Assignment code here
+def q4():
+  #Write Assignment code here
   print("3 x 3 = 9")
   
 q1()
