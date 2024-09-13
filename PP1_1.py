9cd1f4fe974dd6e8bef389cca930884f77d2078d
Eric-Fang-ICD201-01 <eric.fang@student.tdsb.on.ca>
2024-09-13 13:30:48 +0000

    hi

--- PP1_1.py
+++ PP1_1.py
@@ -1,16 +1,18 @@
 def q1():
   #Write Assignment code here
+  print("Hello World")
 
 def q2():
   #Write Assignment code here
-  
+  print(1)
+  print(2)
+  print(3)
+  print(4)
+  print(5)
+
 def q3():
   #Write Assignment code here
-  
-def q4:
-  #Write Assignment code here
+  print(" Help Me!")
 
-q1()
-q2()
-q3()
-q4()
+def q4():
+  #Write Assignment code here
