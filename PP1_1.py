4d4b82ba8052fb75a9d947cecf437b662c51af03
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-09-13 09:44:29 -0400

    Update PP1_1.py

--- PP1_1.py
+++ PP1_1.py
@@ -1,12 +1,15 @@
 def q1():
   #Write Assignment code here
-  print("Hello World")
 def q2():
   #Write Assignment code here
-  print("1\n2\n3\n4\n5")
+
 def q3():
   #Write Assignment code here
-  print(" Help Me!")
+
 def q4:
   #Write Assignment code here
-  print("3 x 3 = 9")
+
+q1()
+q2()
+q3()
+q4()
